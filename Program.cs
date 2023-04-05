9172caee244b901346db4ef069c0a9670860c4cb
ShevaMellany <128024052+shevaamellany@users.noreply.github.com>
2023-04-05 14:08:10 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Taxi taxi = new ClasTaxi.Taxi();
+
+            taxi.DriverName = "jarwo";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
